92c01f8676ad4f4e308612978f67ecba7236eea1
ahlev <ohahlev@gmail.com>
2019-12-30 22:37:24 +0700

    add version into verbose name

--- tag/__init__.py
+++ tag/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.0.2'
+__version__ = '0.0.1'
 default_app_config = 'tag.apps.TagConfig'
--- tag/apps.py
+++ tag/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.html import format_html
+from . import __version__ as VERSION
 
 class TagConfig(AppConfig):
     name = 'tag'
-    verbose_name = 'Tag Management'
+    verbose_name = format_html("Tag Management <span class='version'>{}</span>", VERSION)
