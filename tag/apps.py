02bc295597ec81a7dd69e1b6811d88e02506c862
ahlev <ohahlev@gmail.com>
2020-01-21 00:34:55 +0700

    remove style from version label

--- tag/apps.py
+++ tag/apps.py
@@ -4,4 +4,4 @@ from . import __version__ as VERSION
 
 class TagConfig(AppConfig):
     name = 'tag'
-    verbose_name = format_html("Tag Management <span class='version'>{}</span>", VERSION)
+    verbose_name = format_html("Tag Management {}", VERSION)
